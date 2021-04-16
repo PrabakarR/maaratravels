e1cbc11c2117689a1c030804ec201ea4296c3f57
Prabakar <prabakar@newgendigital.com>
2021-04-16 11:58:03 +0530

    push changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,33 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var scrolltoOffset = $('#header').outerHeight() - 17;
+    $(document).on('click', '.scrollto', function(e) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
+  
+          var scrollto = target.offset().top - scrolltoOffset;
+  
+          if ($(this).attr("href") == '#header') {
+            scrollto = 0;
+          }
+  
+          $('html, body').animate({
+            scrollTop: scrollto
+          }, 1000);
+  
+          if ($(this).parents('.navbar-nav').length) {
+            $('.scrollto').removeClass('active');
+            $(this).closest('li > a').addClass('active');
+          }
+          return false;
+        }
+      }
+    });
+
   }
 
 }
